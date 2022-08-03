b70234905e47f2079683bcc9b8d90f9c51eac39c
Mateja <mateja176@gmail.com>
2022-08-03 16:16:35 +0300

    feat: adds Vec example

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,16 @@ fn main() {
     use List::{Cons, Nil};
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("BoxList: {:?}", list);
+
+    let mut vector = vec![1, 2, 3];
+    println!("Vec last: {:?}", vector.last());
+    vector.push(4);
+    println!("Vec last after push: {:?}", vector.last());
+    vector.remove(vector.len() - 1);
+    println!("Vec last after remove: {:?}", vector.last());
+
+    let product = vector
+        .iter()
+        .fold(0, |acc, element| acc + i32::pow(2, *element));
+    println!("Vec sum of powers of 2: {}", product);
 }
