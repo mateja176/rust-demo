7cd6e4798e5af530c85a617686c8817a44dddd98
Mateja <mateja176@gmail.com>
2022-08-03 14:45:11 +0300

    feat: implements List with Box smart pointer

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,13 @@ use print::greet;
 
 fn main() {
     greet::en("Rust");
+
+    #[derive(Debug)]
+    enum List {
+        Cons(i32, Box<List>),
+        Nil,
+    }
+    use List::{Cons, Nil};
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("BoxList: {:?}", list);
 }
